2d4d8607324c5ecbf611e9184e234e1f3ca022e4
Martin Perez <sebastian187@outlook.es>
2026-01-14 13:51:29 -0500

    adding metadata to the apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,33 @@ plugins {
 
 android {
     namespace = "com.example.mysecureapp"
-    compileSdk = 35 // Ajustado a valor estable estándar para SDK 35/36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mysecureapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        // --- LÓGICA DINÁMICA (DEVSECOPS) ---
+        // 1. Intentamos leer el número de Build desde GitHub Actions.
+        // Si no existe (ej. compilando en local), usamos '1' por defecto.
+        val buildNumber = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 1
+
+        // 2. Leemos el Hash del commit para saber qué código exacto es.
+        // Tomamos solo los primeros 7 caracteres (ej. "a1b2c3d").
+        val commitHash = System.getenv("GITHUB_SHA")?.take(7) ?: "LOCAL-DEV"
+
+        // Asignamos los valores dinámicos al APK
+        versionCode = buildNumber
+        versionName = "1.0.$buildNumber" // Ej: 1.0.42
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // --- INYECCIÓN DE METADATOS
+        // Estos campos se podrán leer desde MainActivity con BuildConfig.NOMBRE_CAMPO
+        buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
+        buildConfigField("String", "CICD_ORIGIN", "\"GitHub Actions\"")
+        buildConfigField("String", "COMMIT_HASH", "\"$commitHash\"")
     }
 
     // PUNTO 5: Configuración de firmado usando variables de entorno
@@ -48,6 +65,11 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        // Habilitamos esto para poder usar la clase BuildConfig en el código Kotlin
+        buildConfig = true
     }
 }
 
